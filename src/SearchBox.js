3c701a88e87b3af29160b92627f915228f4e70f4
ignocius <ignocius@gmail.com>
2019-04-28 17:34:50 +0200

    raw react learning part 4

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import  React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+            <input
+                className="pa3 bg-light-blue b--green"
+                type="search" 
+                placeholde='search robots' />
+        </div>
+    );
+}
+
+export default SearchBox;
